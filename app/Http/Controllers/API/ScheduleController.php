3dd90a7dbd3abd1bb6a5fd0068357d87ac2a61b7
CamilaOliver <olivercamila10@hotmail.com>
2022-04-18 03:49:20 -0300

    first commit

---
+++ app/Http/Controllers/API/ScheduleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Services\ScheduleService;
+
+class DoctorController extends Controller
+{
+    protected $scheduleService;
+
+    public function __construct(ScheduleService $scheduleService)
+    {
+        $this->scheduleService = $scheduleService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $schedules = $this->scheduleService->list();
+
+        return $schedules;
+    }
+
+}
